b1b53786b68fca76650782ec6881054e18e02f54
nikitinalexx <nikitinalexx22@gmail.com>
2019-05-07 00:40:26 +0300

    avl tree complete

--- src/com/alex/nikitin/AvlTree.java
+++ src/com/alex/nikitin/AvlTree.java
@@ -10,7 +10,6 @@ public class AvlTree<T extends Comparable<T>> {
             this.value = value;
         }
 
-
     }
 
     private Node<T> root;
@@ -47,19 +46,74 @@ public class AvlTree<T extends Comparable<T>> {
 
         int loadFactor = getNodeHeight(node.left) - getNodeHeight(node.right);
 
-        if (loadFactor > 2 && node.left.value.compareTo(value) > 0) {
-            return leftLeftCase(node);
+        if (loadFactor > 1) {
+            int result = node.left.value.compareTo(value);
+            if (result > 0) {
+                return rightRotate(node);
+            } else if (result < 0) {
+                node = leftRotate(node);
+                return rightRotate(node);
+            }
+        } else if (loadFactor < -1) {
+            int result = node.right.value.compareTo(value);
+            if (result > 0) {
+                node = rightRotate(node);
+                return leftRotate(node);
+            } else if (result < 0) {
+                return leftRotate(node);
+            }
         }
 
+        return node;
     }
 
-    private Node<T> leftLeftCase(Node<T> z) {
+    private Node<T> rightRotate(Node<T> z) {
         Node<T> y = z.left;
 
         z.left = y.right;
         y.right = z;
 
+        return y;
+    }
+
+    private Node<T> leftRotate(Node<T> z) {
+        Node<T> y = z.right;
+
+        z.right = y.left;
+        y.left = z;
 
         return y;
+    }
+
+    public void traverse() {
+        traverseUtil(root);
+    }
+
+    private void traverseUtil(Node<T> node) {
+        if (node != null) {
+            if (node.left != null) {
+                System.out.println("Left (");
+            }
+            traverseUtil(node.left);
+            if (node.left != null) {
+                System.out.println(")");
+            }
+            System.out.println(node.value);
+            if (node.right != null) {
+                System.out.println("Right (");
+            }
+            traverseUtil(node.right);
+            if (node.right != null) {
+                System.out.println(")");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AvlTree<Integer> avlTree = new AvlTree<>();
+        for (int i = 1; i <= 11; i++) {
+            avlTree.insert(i);
+        }
+        avlTree.traverse();
     }
 }
